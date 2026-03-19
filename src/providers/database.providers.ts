c6a466aba2da75650ac5a0232940281265dd29f0
Chin Guang <ganchinguang0622@gmail.com>
2026-03-19 12:52:03 +0800

    Add Docker support for Postgres and PgAdmin
    This commit introduces a docker-compose.yml file to set up PostgreSQL
    and PgAdmin containers. It also adds database providers and a module in
    the NestJS application to connect to the PostgreSQL database.

---
+++ src/providers/database.providers.ts
@@ -0,0 +1,20 @@
+import { DataSource } from 'typeorm';
+
+export const databaseProviders = [
+  {
+    provide: 'DATA_SOURCE',
+    useFactory: async () => {
+      const dataSource = new DataSource({
+        type: 'postgres',
+        host: process.env.POSTGRES_HOST,
+        port: parseInt(process.env.POSTGRES_PORT, 10) ?? 5432,
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB,
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      });
+
+      return dataSource.initialize();
+    },
+  },
+];
